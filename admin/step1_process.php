578945bd1619ce4b6d7fe37a7725ad1fded7dbf5
Boone B Gorges <boonebgorges@gmail.com>
2016-01-25 11:08:52 -0600

    whitespace

--- admin/step1_process.php
+++ admin/step1_process.php
@@ -38,12 +38,12 @@
 		);
 
             	$this->ssw_log_sql_error($wpdb->last_error);
-		    
+
 		    if( is_wp_error( $result ) ) {
 				$error_string = 'Please select a proper use case for your site';
 				echo '<div id="message" class="error"><p>'.$error_string.'</p></div>';
 		    }
 		}
 	}
-    
+
 ?>
--- admin/step2_process.php
+++ admin/step2_process.php
@@ -5,17 +5,17 @@
         /* sanitize_email sanitizes the value to allowed email address for passing in to a SQL query */
         $admin_email = sanitize_email( $_POST['admin_email'] );
             $this->ssw_debug_log('step2_process', 'admin_email', $admin_email);
-        
+
         /* sanitize_key performs strict sanitization on the value admin_user_id before passing in to a SQL query */
         $admin_user_id = sanitize_key( $_POST['admin_user_id'] );
             $this->ssw_debug_log('step2_process', 'admin_user_id', $admin_user_id);
-        
+
         $site_address_bucket = sanitize_key( $_POST['site_address_bucket'] );
             $this->ssw_debug_log('step2_process', 'site_address_bucket', $site_address_bucket);
-        
+
         $site_address = str_replace( '-', '', sanitize_key( $_POST['site_address'] ));
             $this->ssw_debug_log('step2_process', 'site_address', $site_address);
-        
+
         /* Check if the bucket selected is from the list of all buckets that should be blank buckets */
         if( in_array($site_address_bucket, $site_address_bucket_none_value) != true && $site_address_bucket != '' ) {
             $path = $current_site->path.$site_address_bucket.'-'.$site_address;
@@ -23,11 +23,11 @@
         else {
             $path = $current_site->path.$site_address ;
         }
-        
+
         /* sanitize_title_for_query sanitizes the value to make it safe for passing in to a SQL query */
         $title = sanitize_text_field( $_POST['site_title'] );
             $this->ssw_debug_log('step2_process', 'title', $title);
-        
+
         /* sanitize_key sanitizes the value to all right content required for the path for security */
         /* Multisite Privacy Plugin uses value -1, -2 and -3 hence we add 3 and then subtract 3 after sending it to sanitize values */
         if( isset($_POST['site_privacy'] ) ) {
@@ -72,10 +72,10 @@
 	);
 
             $this->ssw_log_sql_error($wpdb->last_error);
-        
+
         if ( is_wp_error( $result ) ) {
            $error_string = $result->get_error_message();
            echo '<div id="message" class="error"><p>' . $error_string . '</p></div>';
-        }    
+        }
     }
 ?>
